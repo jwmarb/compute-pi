3ed69ae682182c127342a16c4e87150e94746f06
Joseph Marbella <dieptuber@gmail.com>
2024-05-07 17:17:26 -0700

    chore: local development files

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export CMAKE_C_COMPILER=ext/openmpi/bin/mpicc
+export CMAKE_CXX_COMPILER=ext/openmpi/bin/mpi++
--- setup.sh
+++ setup.sh
@@ -6,34 +6,44 @@ CWD=$(pwd)
 EXT=$CWD/ext
 
 # setup
-mkdir $EXT
+mkdir -p $EXT
 
-# Install GMP headers
+# Install GMP
 GMP_VERSION=6.3.0
 cd $EXT
-mkdir gmp
 wget https://ftp.gnu.org/gnu/gmp/gmp-$GMP_VERSION.tar.gz
-tar -xzf ./gmp-$GMP_VERSION.tar.gz -C gmp
+tar -xzf ./gmp-$GMP_VERSION.tar.gz
 rm gmp-$GMP_VERSION.tar.gz
+mv gmp-$GMP_VERSION gmp
 cd gmp
 ./configure
+make -j$(nproc)
 
-# Install MPI headers
-MPI_VERSION=5.0.3
-cd $EXT
-wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-$MPI_VERSION.tar.bz2
-tar -jxpf openmpi-$MPI_VERSION.tar.bz2
-cd openmpi-$MPI_VERSION
-./configure --prefix=$EXT/openmpi
-make install
-cd ..
-rm openmpi-$MPI_VERSION.tar.bz2 # remove compressed file
-rm -rf openmpi-$MPI_VERSION # remove source
 
-# Install FMT headers
+# Install MPI
+# Depending on your HPC configuration, this may not be necessary to install.
+# For local installation on a local machine, uncomment the installation below
+
+# MPI_VERSION=3.1.4
+# cd $EXT
+# wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-$MPI_VERSION.tar.bz2
+# tar -jxpf openmpi-$MPI_VERSION.tar.bz2
+# cd openmpi-$MPI_VERSION
+# ./configure --prefix=$EXT/openmpi
+# make -j$(nproc) all
+# make install
+# cd ..
+# rm openmpi-$MPI_VERSION.tar.bz2 # remove compressed file
+# rm -rf openmpi-$MPI_VERSION # remove source
+
+# Install FMT
 FMT_VERSION=10.2.1
 cd $EXT
-mkdir fmt
 wget https://github.com/fmtlib/fmt/releases/download/$FMT_VERSION/fmt-$FMT_VERSION.zip
-unzip fmt-$FMT_VERSION.zip -d fmt
+unzip fmt-$FMT_VERSION.zip
 rm fmt-$FMT_VERSION.zip
+mv fmt-$FMT_VERSION fmt
+cd fmt
+cmake -DBUILD_SHARED_LIBS=TRUE -S . -B out/build
+cd out/build
+make -j$(nproc)
